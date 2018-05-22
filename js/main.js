88ba776323f290e36cac9fd1b124f963f31260f0
少榮 <lior@ks-design.com.tw>
2018-05-22 14:53:59 +0800

    refactor main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,39 @@
 (function ($) {
-    $.fn.compress = function () {
-        var canvas = this[0];
-        var ctx = canvas.getContext('2d');
-        var cWidth = this.data('width');
 
-        var eleFile = document.getElementById("eleFile");
-        eleFile.addEventListener('change', function (e) {
-            var render = new FileReader();
-            render.addEventListener('load', function (e) {
-                var img = new Image();
-                img.addEventListener('load', function (e) {
-                    canvas.width = cWidth;
-                    var scale = e.target.width / cWidth;
-                    var gHeight = Math.round(e.target.height / scale);
-                    canvas.height = gHeight;
-                    ctx.drawImage(e.target, 0, 0, cWidth, gHeight);
-                })
-                img.src = e.target.result;
+    $.fn.preview = function () {
+
+        for(var i=0; i<this.length;i++)
+        {
+            var eleFile = this[i];
+            eleFile.addEventListener('change', function (e) {
+                var eleFile = e.target;
+                var preview = document.getElementById($(eleFile).data('preview'));
+                $(preview).html('');
+                for(var j=0;j<e.target.files.length;j++)
+                {
+                    var render = new FileReader();
+                    render.addEventListener('load', function (e) {
+                        var img = new Image();
+                        img.addEventListener('load', function (e) {
+                            var canvas = document.createElement('canvas');
+                            var ctx = canvas.getContext('2d');
+                            var cWidth = 400;
+                            var scale = e.target.width / cWidth;
+                            var cHeight = Math.round(e.target.height / scale);
+                            canvas.height = cHeight;
+                            ctx.drawImage(e.target, 0, 0, cWidth, cHeight);
+
+                            $(preview).append(canvas);
+
+                        })
+                        img.src = e.target.result;
+
+                    })
+                    render.readAsDataURL(e.target.files[j]);
+                }
 
             })
-            render.readAsDataURL(e.target.files[0]);
-        })
-        return this;
+
+        }
     }
-
-
-
 })(jQuery)
