c620c2df97da966e16c87bea3c699248d2707615
少榮 <lior@ks-design.com.tw>
2018-06-02 00:10:18 +0800

    fix mousemove

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 (function ($) {
-    $(function () {
-        var picker = document.getElementsByClassName('picker')[0];
-        picker.addEventListener('mousemove',function (e) {
-
-        })
-    })
-
     $.fn.preview = function () {
 
         for(var i=0; i<this.length;i++)
@@ -31,8 +24,21 @@
                             ctx.drawImage(e.target, 0, 0, cWidth, cHeight);
 
                             $(preview).append(canvas);
-                            document.addEventListener('mousemove',canvasMouseMove);
 
+                            canvas.addEventListener('cmousemove',canvasMouseMove);
+                            canvas.addEventListener('mousemove',function (e) {
+                                // console.log(e.target.offsetLeft,e.target.offsetTop);
+                                var cEvent = new CustomEvent('cmousemove',{'detail':{mouseX:e.target.offsetLeft+e.offsetX,
+                                                                                    mouseY:e.target.offsetTop+e.offsetY}});
+                                canvas.dispatchEvent(cEvent);
+                            });
+                            var picker = document.getElementsByClassName('picker')[0];
+                            picker.addEventListener('mousemove',function (e) {
+
+                                var cEvent = new CustomEvent('cmousemove',{'detail':{mouseX:e.target.offsetLeft+e.offsetX,
+                                    mouseY:e.target.offsetTop+e.offsetY}});
+                                canvas.dispatchEvent(cEvent);
+                            })
 
                         })
                         img.src = e.target.result;
@@ -46,13 +52,15 @@
         }
     }
 
+
     function canvasMouseMove(e) {
-        console.log(`${e.offsetX}, ${e.offsetY}`);
-        console.log(`${e.target.offsetLeft}, ${e.target.offsetTop}`);
+        console.log(`${e.detail.mouseX},${e.detail.mouseX}`);
+        // console.log(`${e.offsetX}, ${e.offsetY}`);
+        // console.log(`${e.target.offsetLeft}, ${e.target.offsetTop}`);
         var $picker = $('.picker').eq(0);
-        console.log(`${$picker.width()},${$picker.height()}`)
-        $picker.eq(0).css('left', (e.target.offsetLeft+e.offsetX-parseInt($picker.width()/2))+'px');
-        $picker.eq(0).css('top', (e.target.offsetTop+e.offsetY-parseInt($picker.height()/2))+'px');
+        // console.log(`${$picker.width()},${$picker.height()}`)
+        $picker.eq(0).css('left', (e.detail.mouseX-parseInt($picker.width()/2))+'px');
+        $picker.eq(0).css('top', (e.detail.mouseY-parseInt($picker.height()/2))+'px');
 
     }
 })(jQuery)
