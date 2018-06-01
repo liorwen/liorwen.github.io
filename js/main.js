c8ba6aa1f3b2d671905de2444027471767501611
少榮 <lior@ks-design.com.tw>
2018-06-01 23:23:24 +0800

    update main

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 (function ($) {
 
+
     $.fn.preview = function () {
 
         for(var i=0; i<this.length;i++)
@@ -25,6 +26,7 @@
                             ctx.drawImage(e.target, 0, 0, cWidth, cHeight);
 
                             $(preview).append(canvas);
+                            canvas.addEventListener('mousemove',canvasMouseMove);
 
                         })
                         img.src = e.target.result;
@@ -36,5 +38,15 @@
             })
 
         }
+    }
+
+    function canvasMouseMove(e) {
+        console.log(`${e.offsetX}, ${e.offsetY}`);
+        console.log(`${e.target.offsetLeft}, ${e.target.offsetTop}`);
+        var $picker = $('.picker').eq(0);
+        console.log(`${$picker.width()},${$picker.height()}`)
+        $picker.eq(0).css('left', (e.target.offsetLeft+e.offsetX-parseInt($picker.width()/2))+'px');
+        $picker.eq(0).css('top', (e.target.offsetTop+e.offsetY-parseInt($picker.height()/2))+'px');
+
     }
 })(jQuery)
