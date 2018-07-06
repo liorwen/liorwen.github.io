deba11038ff4262d257147fc388dddb03f0f86a0
少榮 <lior@ks-design.com.tw>
2018-07-06 18:17:23 +0800

    add fb app

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@
                                     } else {
                                         console.log('User cancelled login or did not fully authorize.');
                                     }
-                                }, {scope: 'email,user_photos,user_posts,publish_pages'});
+                                }, {scope: 'email,user_photos,user_posts,manage_pages,publish_pages'});
                             })
 
                         })
