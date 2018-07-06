1a860f42b53be5bc62cbf683d57e0ca36e801546
少榮 <lior@ks-design.com.tw>
2018-07-06 16:52:56 +0800

    add fb app

--- js/main.js
+++ js/main.js
@@ -69,7 +69,16 @@
             FB.getLoginStatus(function(response) {
                 if(response.status === 'connected')
                 {
-                    FB.login();
+                    FB.login(function(response) {
+                        if (response.authResponse) {
+                            console.log('Welcome!  Fetching your information.... ');
+                            FB.api('/me', function(response) {
+                                console.log('Good to see you, ' + response.name + '.');
+                            });
+                        } else {
+                            console.log('User cancelled login or did not fully authorize.');
+                        }
+                    });
                 }
             });
             return false;
